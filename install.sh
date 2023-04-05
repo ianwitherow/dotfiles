6f92ce37d31891d308ef51dc03fcbca114e8f421
Ian Witherow <ian.witherow@state.co.us>
2023-04-05 13:41:13 -0600

    Adds command to load nvm after installing it

--- install.sh
+++ install.sh
@@ -143,6 +143,11 @@ if [ -d "$HOME/.nvm" ]; then
 else
 	echo "NVM not found. Installing..."
 	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
+
+	export NVM_DIR="$HOME/.nvm"
+	[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+	[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 	# install latest stable version of Node.js and npm
 	nvm install node
 	echo "Node.js and npm installation complete."
